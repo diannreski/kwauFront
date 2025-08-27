4838597b66e51096dd921d7a3a5785c021a27d1e
diannreski <reskidian73@gmail.com>
2025-08-27 18:33:08 +0900

    add env

--- src/services/apiService.js
+++ src/services/apiService.js
@@ -1,7 +1,7 @@
 // API Service for Frontend
 // File: src/services/apiService.js
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = process.env.REACT_APP_API_URL;
 
 /**
  * Make API request to backend
